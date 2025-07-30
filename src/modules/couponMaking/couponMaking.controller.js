b743f099415f1d7f898018bd3b252ae9c15d3638
Sajjad177 <sajjadhossainx06@gmail.com>
2025-07-30 16:24:31 +0600

    Implement CRUD operations for coupons in the couponMaking module

--- src/modules/couponMaking/couponMaking.controller.js
+++ src/modules/couponMaking/couponMaking.controller.js
@@ -16,8 +16,94 @@ const createCoupon = async (req, res) => {
   }
 };
 
+const getAllCoupon = async (req, res) => {
+  try {
+    const result = await couponMaking.find({});
+
+    return res.status(200).json({
+      success: true,
+      message: "Coupon fetched successfully",
+      data: result,
+    });
+  } catch (error) {
+    return res
+      .status(400)
+      .json({ success: false, message: error.message, error });
+  }
+};
+
+const getSingleCoupon = async (req, res) => {
+  try {
+    const { couponId } = req.params;
+    const coupon = await couponMaking.findById(couponId);
+    if (!coupon) {
+      throw new Error("Coupon not found");
+    }
+
+    const result = await couponMaking.findById(couponId);
+
+    return res.status(200).json({
+      success: true,
+      message: "Coupon get successfully",
+      data: result,
+    });
+  } catch (error) {
+    return res
+      .status(400)
+      .json({ success: false, message: error.message, error });
+  }
+};
+
+const updateCoupon = async (req, res) => {
+  try {
+    const { couponId } = req.params;
+    const coupon = await couponMaking.findById(couponId);
+    if (!coupon) {
+      throw new Error("Coupon not found");
+    }
+    const result = await couponMaking.findByIdAndUpdate(couponId, req.body, {
+      new: true,
+    });
+
+    return res.status(200).json({
+      success: true,
+      message: "Coupon updated successfully",
+      data: result,
+    });
+  } catch (error) {
+    return res
+      .status(400)
+      .json({ success: false, message: error.message, error });
+  }
+};
+
+const deleteCoupon = async (req, res) => {
+  try {
+    const { couponId } = req.params;
+    const coupon = await couponMaking.findById(couponId);
+    if (!coupon) {
+      throw new Error("Coupon not found");
+    }
+    const result = await couponMaking.findByIdAndDelete(couponId);
+
+    return res.status(200).json({
+      success: true,
+      message: "Coupon deleted successfully",
+      data: result,
+    });
+  } catch (error) {
+    return res
+      .status(400)
+      .json({ success: false, message: error.message, error });
+  }
+};
+
 const couponMakingController = {
   createCoupon,
+  getAllCoupon,
+  getSingleCoupon,
+  updateCoupon,
+  deleteCoupon,
 };
 
 module.exports = couponMakingController;
--- src/modules/couponMaking/couponMaking.router.js
+++ src/modules/couponMaking/couponMaking.router.js
@@ -4,6 +4,12 @@ const couponMakingController = require("./couponMaking.controller");
 const router = Router();
 
 router.post("/create", couponMakingController.createCoupon);
+router.get("/", couponMakingController.getAllCoupon);
+router.get("/:couponId", couponMakingController.getSingleCoupon);
+
+router.put("/update/:couponId", couponMakingController.updateCoupon);
+
+router.delete("/delete/:couponId", couponMakingController.deleteCoupon);
 
 const couponMakingRouter = router;
 module.exports = couponMakingRouter;
