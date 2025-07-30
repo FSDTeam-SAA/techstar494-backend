e3cc5461fe5faa636d6729cbd16bb0d05685b000
Sajjad177 <sajjadhossainx06@gmail.com>
2025-07-30 11:04:27 +0600

    Refactor legal document schema and remove privacy, refund policy, and terms and conditions modules

--- src/modules/legalDocuments/legalDocuments.model.js
+++ src/modules/legalDocuments/legalDocuments.model.js
@@ -1,22 +1,24 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const legalDocumentSchema = new mongoose.Schema({
-    documentType: {
-        type: String,
-        required: true,
-        enum: ['privacy_policy', 'terms_conditions', 'legality', 'refund_policy'],
-        unique: true
-    },
-    content: {
-        type: String,
-        required: true
-    },
-    lastUpdated: {
-        type: Date,
-        default: Date.now
-    }
+  documentType: {
+    type: String,
+    required: true,
+    enum: ["privacy_policy", "terms_conditions", "legality", "refund_policy"],
+    unique: true,
+  },
+  content: {
+    type: String,
+    required: true,
+  },
+  lastUpdated: {
+    type: Date,
+    default: Date.now,
+  },
 });
 
-const LegalDocument = mongoose.models.LegalDocument || mongoose.model('LegalDocument', legalDocumentSchema);
+const LegalDocument =
+  mongoose.models.LegalDocument ||
+  mongoose.model("LegalDocument", legalDocumentSchema);
 
 module.exports = LegalDocument;
