cec443726e3f148b4f25213f97cb58fc49f59ca1
Sajjad Hossain <155253869+Sajjad177@users.noreply.github.com>
2025-08-11 01:52:38 +0600

    auth gurd is added

--- src/modules/couponMaking/couponMaking.router.js
+++ src/modules/couponMaking/couponMaking.router.js
@@ -1,15 +1,29 @@
 const { Router } = require("express");
 const couponMakingController = require("./couponMaking.controller");
+const auth = require("../../middleware/auth");
+const USER_ROLE = require("../user/user.constant");
 
 const router = Router();
 
-router.post("/create", couponMakingController.createCoupon);
+router.post(
+  "/create",
+  auth(USER_ROLE.admin),
+  couponMakingController.createCoupon
+);
 router.get("/", couponMakingController.getAllCoupon);
 router.get("/:couponId", couponMakingController.getSingleCoupon);
 
-router.put("/update/:couponId", couponMakingController.updateCoupon);
+router.put(
+  "/update/:couponId",
+  auth(USER_ROLE.admin),
+  couponMakingController.updateCoupon
+);
 
-router.delete("/delete/:couponId", couponMakingController.deleteCoupon);
+router.delete(
+  "/delete/:couponId",
+  auth(USER_ROLE.admin),
+  couponMakingController.deleteCoupon
+);
 
 const couponMakingRouter = router;
 module.exports = couponMakingRouter;
--- src/modules/legalDocuments/legalDocuments.router.js
+++ src/modules/legalDocuments/legalDocuments.router.js
@@ -7,16 +7,18 @@ const {
   legality,
   updateDocument,
 } = require("../../modules/legalDocuments/legalDocuments.controller");
+const auth = require("../../middleware/auth");
+const USER_ROLE = require("../user/user.constant");
 
 const router = express.Router();
 
-router.post("/create", createDocument);
+router.post("/create", auth(USER_ROLE.admin), createDocument);
 router.get("/privacy-policy", getPrivacyPolicy);
 router.get("/terms-conditions", termsConditions);
 router.get("/legality", legality);
 router.get("/refund-policy", refundPolicy);
 
-router.put("/update/:id", updateDocument);
+router.put("/update/:id", auth(USER_ROLE.admin), updateDocument);
 
 const legalDocumentsRouter = router;
 module.exports = legalDocumentsRouter;
