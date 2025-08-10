c2448ef0e7f2a900137848edf961465f9d7657df
Sajjad Hossain <155253869+Sajjad177@users.noreply.github.com>
2025-08-11 01:55:57 +0600

    auth added

--- src/modules/category/category.router.js
+++ src/modules/category/category.router.js
@@ -7,24 +7,28 @@ const {
   deleteCategory,
 } = require("../category/category.controller");
 const { upload } = require("../../utils/cloudnary");
+const auth = require("../../middleware/auth");
+const USER_ROLE = require("../user/user.constant");
 
 const router = express.Router();
 
 router.post(
   "/",
   upload.single("image"), // Single image upload
+  auth(USER_ROLE.admin),
   createCategory
 );
 
-router.get("/", getCategories);
-router.get("/:id", getCategoryById);
+router.get("/", auth(USER_ROLE.admin), getCategories);
+router.get("/:id", auth(USER_ROLE.admin), getCategoryById);
 
 router.put(
   "/:id",
   upload.single("image"), // Single image upload for update
+  auth(USER_ROLE.admin),
   updateCategory
 );
 
-router.delete("/:id", deleteCategory);
+router.delete("/:id", auth(USER_ROLE.admin), deleteCategory);
 
 module.exports = router;
